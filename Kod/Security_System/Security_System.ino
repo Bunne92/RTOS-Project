68c1c40be5fac0995b6eb5d23bdcc2b465b9a03b
Unknown <dennisbunne92@gmail.com>
2017-11-01 15:57:29 +0100

    Final code

--- Kod/Security_System/Security_System.ino
+++ Kod/Security_System/Security_System.ino
@@ -1,10 +1,7 @@
-//#include <wirish/wirish.h>
-//#include "libraries/FreeRTOS/MapleFreeRTOS.h"
 #include <MapleFreeRTOS900.h>
 #include "Arduino.h"
 #include "Wire.h"
 #include "uRTCLib.h"
-uRTCLib rtc;
 
 #define pin1 PB13
 #define pin2 PB8
@@ -14,24 +11,19 @@ uRTCLib rtc;
 #define pin6 PB15
 #define pin7 PB14
 
-
-#define One (digitalRead(pin2)&&digitalRead(pin3))
-#define Two (digitalRead(pin1)&&digitalRead(pin2))
-#define Three (digitalRead(pin5)&&digitalRead(pin2))
-#define Four (digitalRead(pin3)&&digitalRead(pin7))
-#define Five (digitalRead(pin7)&&digitalRead(pin1))
-#define Six (digitalRead(pin5)&&digitalRead(pin7))
-#define Seven (digitalRead(pin3)&&digitalRead(pin6))
-#define Eight (digitalRead(pin1)&&digitalRead(pin6))
-#define Nine (digitalRead(pin5)&&digitalRead(pin6))
-#define Zero (digitalRead(pin1)&&digitalRead(pin4))
-#define Star (digitalRead(pin3)&&digitalRead(pin4))
-#define Square (digitalRead(pin5)&&digitalRead(pin4))
+uRTCLib rtc;
 SemaphoreHandle_t xSemaphore=NULL;
 
-unsigned int pos;
 bool PIR;
+bool ManualONWeekend = false;
+bool ManualONWorkNight = false;
+bool ManualONWorkday = false;
+
 int RealTimeMin=0;
+char Password[4]={'1','2','3','4'};
+char KeyInput[4]={'0','0','0','0'};
+int KeyStep=0;
+
 struct LarmSchedule {
   int StartTime;
   int EndTime;
@@ -42,72 +34,128 @@ struct LarmSchedule {
  struct LarmSchedule *Workday = (struct LarmSchedule *)&WeekSchedule[0];
  struct LarmSchedule *Weekend = (struct LarmSchedule *)&WeekSchedule[1];
 
+//This task read inputs and put it in KeyInput[], when 4 inputs is registered then a check will trigger and compare 
+//KeyInput[4] with Password[4], if a match then ManualONWeekend or ManualONWorkNight or ManualONWorkday will change 
+//status from false to true or the opposite. Depending on the time and day of the week a specific "ManualOn" will change .
 static void KeypadTask(void *pvParameters) {
-  for(;;){
-        //if(!One){Serial.println("=1=");}
-        //if(!Two){Serial.println("=2=");}
-        //if(!Three){Serial.println("=3=");}
-        //if(!Four){Serial.println("=4=");}
-        if(!Five){Serial.println("=5=");}
-        if(!Six){Serial.println("=6=");}
-        //if(!Seven){Serial.println("=7=");}
-        //if(!Eight){Serial.println("=8=");}
-        //if(!Nine){Serial.println("=9=");}
-        //if(!Zero){Serial.println("=0=");}
-        //if(!Star){Serial.println("=*=");}
-        //if(!Square){Serial.println("=#=");}
-  vTaskDelay(1000);
-  
-  }
+      for(;;){
+            digitalWrite(pin7, LOW);
+            digitalWrite(pin6, LOW);
+            digitalWrite(pin4, LOW);
+            digitalWrite(pin2, HIGH);
+            if(digitalRead(pin3) == HIGH){KeyInput[KeyStep]='1';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin1) == HIGH){KeyInput[KeyStep]='2';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin5) == HIGH){KeyInput[KeyStep]='3';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            digitalWrite(pin2, LOW);
+            digitalWrite(pin7, HIGH);
+            if(digitalRead(pin3) == HIGH){KeyInput[KeyStep]='4';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin1) == HIGH){KeyInput[KeyStep]='5';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin5) == HIGH){KeyInput[KeyStep]='6';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            digitalWrite(pin7, LOW);
+            digitalWrite(pin6, HIGH);
+            if(digitalRead(pin3) == HIGH){KeyInput[KeyStep]='7';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin1) == HIGH){KeyInput[KeyStep]='8';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin5) == HIGH){KeyInput[KeyStep]='9';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            digitalWrite(pin6, LOW);
+            digitalWrite(pin4, HIGH);
+            if(digitalRead(pin3) == HIGH){KeyInput[KeyStep]='*';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin1) == HIGH){KeyInput[KeyStep]='0';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            if(digitalRead(pin5) == HIGH){KeyInput[KeyStep]='#';KeyStep++;digitalWrite(PB10, HIGH);vTaskDelay(500);}
+            digitalWrite(pin4, LOW);
+            digitalWrite(PB10, LOW);
+            vTaskDelay(100);
+            if(KeyStep>=4){
+              digitalWrite(PC13, HIGH);
+              vTaskDelay(200);
+              digitalWrite(PC13, LOW);  
+                  KeyStep=0;
+                  for(int ix=0;ix<4;ix++){
+                        if(KeyInput[ix]==Password[ix]){
+                              if (ix==3 && ManualONWorkday==false && (Workday->StartTime)<RealTimeMin && (Workday->EndTime)>RealTimeMin){
+                                    ManualONWorkday = true;
+                              }
+                              else if(ix==3 && ManualONWorkday==true && (Workday->StartTime)<RealTimeMin && (Workday->EndTime)>RealTimeMin){
+                                    ManualONWorkday = false;
+                              }
+                              if (ix==3 && ManualONWorkNight==false &&(Workday->EndTime)<=RealTimeMin){
+                                    ManualONWorkNight = true;
+                              }
+                              else if(ix==3 && ManualONWorkNight==true && (Workday->EndTime)<=RealTimeMin){
+                                    ManualONWorkNight = false;
+                              }
+                              
+                              if (ix==3 && ManualONWeekend==false && (Weekend->StartTime)<RealTimeMin && (Weekend->EndTime)>RealTimeMin){
+                                    ManualONWeekend = true;
+                              }
+                              else if(ix==3 && ManualONWeekend==true && (Weekend->StartTime)<RealTimeMin && (Weekend->EndTime)>RealTimeMin){
+                                    ManualONWeekend = false;
+                              }
+                        }
+                        else{
+                              break;
+                        }
+                  }
+            }
+      }
 }
+//Buzzer will go On if PIR have a value of 1
 static void BuzzerTask(void *pvParameters) {
-  for(;;){
-        if(PIR){
-              //digitalWrite(PB13, HIGH);
-              Serial.println("PIIIIIIIP");
-        }
-        else{
-          //digitalWrite(PB13, LOW);
-          Serial.println("FAIL");
-        }
-        vTaskDelay(1000);
-  }    
+      for(;;){
+            if(PIR){
+                  digitalWrite(PB11, HIGH); //if PIR=1 buzzer on
+            }
+            vTaskDelay(1000);
+      }    
 }     
+//If xSemaphoreTake then Depending on Day of Week Compares real time (in min) with a predetermend schedule and ManualOn to see if larm is on or off.
+//if on then read PIR else if off shut down buzzer and set PIR to 0 then xSemaphoreTakeGive
 static void SensorTask(void *pvParameters) {
-        for(;;){
-              if(xSemaphoreTake( xSemaphore, ( TickType_t ) 10 ) == pdTRUE){
-                    if(rtc.dayOfWeek()<=6 && rtc.dayOfWeek() > 1){
-                    
-                          if((Workday->StartTime)<RealTimeMin && (Workday->EndTime)>RealTimeMin){
-                                //larm av
-                                Serial.println("Larm Av ja");
-                                Serial.println(PIR);Serial.println(PIR);
+      for(;;){
+            if(xSemaphoreTake( xSemaphore, ( TickType_t ) 10 ) == pdTRUE){
+                  if(rtc.dayOfWeek()<=6 && rtc.dayOfWeek() > 1){
+                        if((Workday->StartTime)<RealTimeMin && (Workday->EndTime)>RealTimeMin && ManualONWorkday==false){
+                              //Larm Off
+                              digitalWrite(PB11, LOW); 
+                              PIR=0;
+                              Serial.println(PIR);
+                        }
+                        else if ((Workday->EndTime)<=RealTimeMin && ManualONWorkNight==true){
+                              //Larm Off
+                              digitalWrite(PB11, LOW);
+                              PIR=0;
+                              Serial.println(PIR);
                           }
-                          else{
-                                //larm på
-                                Serial.println("Larm På");
-                                PIR=digitalRead(PB12);
-                                Serial.println(PIR);
+                        else {
+                              //Larm On
+                              PIR=digitalRead(PB12);
+                              Serial.println(PIR);
+                        }
+                  }
+                  if(rtc.dayOfWeek()==7 || rtc.dayOfWeek() == 1){
+                        if((Weekend->StartTime)<RealTimeMin && (Weekend->EndTime)>RealTimeMin && ManualONWeekend==false){
+                              //Larm Off
+                              digitalWrite(PB11, LOW);
+                              PIR=0;
+                              Serial.println(PIR);
+                        }
+                        else if ((Workday->EndTime)<RealTimeMin && ManualONWeekend==true){
+                              //Larm Off 
+                              digitalWrite(PB11, LOW);
+                              PIR=0;
+                              Serial.println(PIR);
                           }
-                    }
-                    if(rtc.dayOfWeek()==7 || rtc.dayOfWeek() == 1){
-                          if((Weekend->StartTime)<RealTimeMin && (Weekend->EndTime)>RealTimeMin){
-                                //larm av
-                                Serial.println("Larm Av");
-                                Serial.println(PIR);
-                          }
-                          else{
-                                //larm på
-                                Serial.println("Larm På");
-                                PIR=digitalRead(PB12);
-                                Serial.println(PIR);
-                          }          
-                    }
-              xSemaphoreGive( xSemaphore );
-              }
-              vTaskDelay(1000);
-        }
+                        else{
+                              //Larm On
+                              PIR=digitalRead(PB12);
+                              Serial.println(PIR);
+                        }          
+                  }
+            xSemaphoreGive( xSemaphore );
+            }
+            vTaskDelay(1000);
+      }
 }
+//if xSemaphoreTake then read new time in minutes then give xSemaphore
 static void ClockTask(void *pvParameters) {      
     for(;;){
         rtc.refresh();
@@ -121,82 +169,63 @@ static void ClockTask(void *pvParameters) {
 }
 
 void setup() {
-    pinMode(pin7, INPUT);   // pin7
-    pinMode(pin6, INPUT);   // pin6
-    pinMode(pin5, INPUT);    // pin5
-    pinMode(pin4, INPUT);    // pin4
-    pinMode(pin3, INPUT);    // pin3
-    pinMode(pin2, INPUT);    // pin2
-    pinMode(pin1, INPUT);  // pin1
-    
-    pinMode(PB12, INPUT);
-    //pinMode(PB13, OUTPUT);
-    Serial.begin(115200);
+      pinMode(pin7, OUTPUT);    // pin7 in Keypad
+      pinMode(pin6, OUTPUT);    // pin6 in Keypad
+      pinMode(pin5, INPUT);     // pin5 in Keypad
+      pinMode(pin4, OUTPUT);    // pin4 in Keypad
+      pinMode(pin3, INPUT);     // pin3 in Keypad
+      pinMode(pin2, OUTPUT);    // pin2 in Keypad
+      pinMode(pin1, INPUT);     // pin1 in Keypad
+      pinMode(PB12, INPUT);     // PIR
+      pinMode(PB11, OUTPUT);    // Buzzer
+      pinMode(PC13, OUTPUT);     // LED
+      pinMode(PB10, OUTPUT);     // LED
+      Serial.begin(115200);
 
-      for(pos = 0; pos < 1000; pos++) {
-    rtc.eeprom_write(pos, (unsigned char) pos % 256);
-     }
-      pos = 0;
-
-// Only used once, then disabled
-//rtc.set(0, 45, 11, 3, 25, 10, 17);
-//  RTCLib::set(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year)
-
-  #ifdef _VARIANT_ARDUINO_STM32_
-  Serial.println("Board: STM32");
-  #else
-  Serial.println("Board: Other");
-  #endif
-  rtc.set_rtc_address(0x68);
-  rtc.set_ee_address(0x57);
-  xSemaphore = xSemaphoreCreateBinary();
-  xSemaphoreGive( xSemaphore );
-  xTaskCreate(BuzzerTask,
-                "Buzzer",
-                configMINIMAL_STACK_SIZE,
-                NULL,
-                tskIDLE_PRIORITY + 2,
-                NULL);
-   xTaskCreate(SensorTask,
-                "PIR",
-                configMINIMAL_STACK_SIZE,
-                NULL,
-                tskIDLE_PRIORITY + 2,
-                NULL);
-   xTaskCreate(ClockTask,
-                "RTC",
-                configMINIMAL_STACK_SIZE,
-                NULL,
-                tskIDLE_PRIORITY + 2,
-                NULL);
-   xTaskCreate(KeypadTask,
-                "Keypad",
-                configMINIMAL_STACK_SIZE,
-                NULL,
-                tskIDLE_PRIORITY + 2,
-                NULL);
-    vTaskStartScheduler();
+      //Set peramitors for RTC
+      for(KeyStep = 0; KeyStep < 1000; KeyStep++) {
+          rtc.eeprom_write(KeyStep, (unsigned char) KeyStep % 256);
+      }
+      KeyStep = 0;
+      // Only used once, then disable. Set the time and date for RTC
+      //rtc.set(0, 32, 12, 2, 30, 10, 17);
+      //  RTCLib::set(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year)
+      #ifdef _VARIANT_ARDUINO_STM32_
+      Serial.println("Board: STM32");
+      #else
+      Serial.println("Board: Other");
+      #endif
+      rtc.set_rtc_address(0x68);
+      rtc.set_ee_address(0x57);
+      xSemaphore = xSemaphoreCreateBinary();
+      xSemaphoreGive( xSemaphore );
+      
+      xTaskCreate(BuzzerTask,
+                  "Buzzer",
+                  configMINIMAL_STACK_SIZE,
+                  NULL,
+                  tskIDLE_PRIORITY + 2,
+                  NULL);
+      xTaskCreate(SensorTask,
+                  "PIR",
+                  configMINIMAL_STACK_SIZE,
+                  NULL,
+                  tskIDLE_PRIORITY + 2,
+                  NULL);
+      xTaskCreate(ClockTask,
+                  "RTC",
+                  configMINIMAL_STACK_SIZE,
+                  NULL,
+                  tskIDLE_PRIORITY + 2,
+                  NULL);
+      xTaskCreate(KeypadTask,
+                  "Keypad",
+                  configMINIMAL_STACK_SIZE,
+                  NULL,
+                  tskIDLE_PRIORITY + 2,
+                  NULL);
+      vTaskStartScheduler();
 }
-
 void loop() {
     // Insert background code here
 }
-
-      
-  /*Serial.print("RTC DateTime: ");
-  Serial.print(rtc.year());
-  Serial.print('/');
-  Serial.print(rtc.month());
-  Serial.print('/');
-  Serial.print(rtc.day());
-  Serial.print(' ');
-  Serial.print(rtc.hour());
-  Serial.print(':');
-  Serial.print(rtc.minute());
-  Serial.print(':');
-  Serial.print(rtc.second());
-  Serial.print(" DOW: ");
-  Serial.print(rtc.dayOfWeek());
-   Serial.println();
-
- */
